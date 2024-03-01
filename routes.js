49d8188a8ce9a38fc176a8ca5f174cb8342c7de9
aenesu <aenesu@outlook.com>
2024-03-01 18:34:42 +0300

    update

--- routes.js
+++ routes.js
@@ -5,11 +5,26 @@
 
 export const DEFAULT_REDIRECT_PAGE = "/dashboard";
 
-
 /**
- * array of routes that are accessible to the public,
- * these routes doesn't require authentication
+ * array of routes that are accessible to the public
+ * These routes does not require authentication
  * @type {string[]}
  */
 
-export const publicRoutes = ["/"]
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+
+export const authenticationRoutes = ["/login", "/error"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const apiPrefix = "/api/auth";
