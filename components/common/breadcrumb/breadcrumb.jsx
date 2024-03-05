ab00714f29a7387c96de5792b8bd640692c164d5
aenesu <aenesu@outlook.com>
2024-03-05 21:08:54 +0300

    update

--- components/common/breadcrumb/breadcrumb.jsx
+++ components/common/breadcrumb/breadcrumb.jsx
@@ -1,15 +1,37 @@
-"use client"
+"use client";
 
-import { useSelectedLayoutSegment } from "next/navigation";
+import { useSelectedLayoutSegments } from "next/navigation";
 import styles from "./breadcrumb.module.scss";
+import Link from "next/link";
 
 export default function Breadcrumb() {
-  const segment = useSelectedLayoutSegment();
-  return (
-    <nav aria-label="breadcrumb">
-      <ul className={styles.breadcrumbList}>
-        {/* Breadcrumb Items*/}
-      </ul>
-    </nav>
-  )
+    const segments = useSelectedLayoutSegments();
+    const filteredSegments = segments.filter(
+        (segment) =>
+            !segment.startsWith("(") &&
+            !segment.endsWith(")") &&
+            segment !== "manage"
+    );
+
+    const breadcrumbItems = filteredSegments.map((segment, index) => {
+        const formattedSegment = segment.replace(/-/g, " ");
+        const path = `/${filteredSegments.slice(0, index + 1).join("/")}`;
+
+        return (
+            <li key={index} className={styles.breadcrumbItem}>
+                <Link
+                    href={path}
+                    title={`Go to ${formattedSegment}`}
+                    className={styles.breadcrumbLink}>
+                    {formattedSegment}
+                </Link>
+            </li>
+        );
+    });
+
+    return (
+        <nav aria-label="breadcrumb">
+            <ul className={styles.breadcrumbList}>{breadcrumbItems}</ul>
+        </nav>
+    );
 }
