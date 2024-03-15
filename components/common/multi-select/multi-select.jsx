88ef478ecab9448f276d05c506d21048fe58ee47
aenesu <aenesu@outlook.com>
2024-03-15 17:17:09 +0300

    update

--- components/common/multi-select/multi-select.jsx
+++ components/common/multi-select/multi-select.jsx
@@ -25,7 +25,30 @@ export default function MultiSelect({ data, defaultValues, name, title }) {
                     : `Select ${title}`}
             </button>
 
-            {isDropdownOpen && <div className={styles.panel}></div>}
+            {isDropdownOpen && (
+                <div className={styles.panel}>
+                    {data.map((item) => (
+                        <fieldset
+                            key={item.value}
+                            className={styles.inputGroup}>
+                            <input
+                                type="checkbox"
+                                id={item.value}
+                                checked={selectedItems.includes(item.value)}
+                                onChange={() =>
+                                    toggleItemSelection(
+                                        item.value,
+                                        setSelectedItems
+                                    )
+                                }
+                            />
+                            <label htmlFor={item.value} title={item.label}>
+                                {item.label}
+                            </label>
+                        </fieldset>
+                    ))}
+                </div>
+            )}
         </fieldset>
     );
 }
